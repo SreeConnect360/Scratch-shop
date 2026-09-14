85c0ec163bf8dc7a2256407c76d436a8e1415e51
SreeConnect <sreeconnect360@gmail.com>
2026-09-14 20:47:22 +0530

    refactor(shiprocket): remove unused HttpMethod import

--- backend-spring-boot/src/main/java/com/reevibes/ai/service/ShiprocketService.java
+++ backend-spring-boot/src/main/java/com/reevibes/ai/service/ShiprocketService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
